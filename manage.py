477ed5c9b71d9dd651f134b635b8dd838a69fa93
tomwen <wen66@126.com>
2016-02-19 09:39:03 +0800

    加入命令管理

--- manage.py
+++ manage.py
@@ -1,2 +1,12 @@
 # coding=utf-8
 from __future__ import (unicode_literals, absolute_import)
+
+from flask_script import Manager, Server
+from app import app
+
+manager = Manager(app)
+
+manager.add_command("runserver", Server(host='0.0.0.0', port=5000, use_debugger=True))
+
+if __name__ == '__main__':
+    manager.run()
